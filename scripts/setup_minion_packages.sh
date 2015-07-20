3aae28e2010b47fcecd13c9af0b70ddcd7e177a4
jhou <jhou@redhat.com>
2015-07-20 11:12:32 +0800

    shell scripts to setup client

---
+++ scripts/setup_minion_packages.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo 'Please provide mininon hostname or ip'
+    exit 1
+fi
+
+minion=$1
+sshopts="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+
+ssh $sshopts root@$minion <<EOF
+yum install nfs-utils glusterfs glusterfs-fuse attr -y
+echo '10.66.79.155 nfs-server' >> /etc/hosts
+echo '10.66.79.108 glusterfs-node01' >> /etc/hosts
+echo '10.66.79.154 glusterfs-node02' >> /etc/hosts
+EOF
