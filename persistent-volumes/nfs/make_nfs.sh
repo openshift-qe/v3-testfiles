d5674302fba2b8b220b71eead0c0ac127d71bde5
jhou <jhou@redhat.com>
2015-10-12 16:14:57 +0800

    fix issue

--- persistent-volumes/nfs/make_nfs.sh
+++ persistent-volumes/nfs/make_nfs.sh
@@ -8,15 +8,15 @@ if [ ! ${UID} -eq 0 ]; then
     exit 1
 fi
 
-# Make volume
-mkdir /nfs
-chown -R nfsnobody:nfsnobody /nfs
-chmod 700 -R /nfs
-
 # Install packages and start services
 yum -y install nfs-utils nfs-utils-lib
 systemctl start rpcbind
 systemctl start nfs
+
+# Make volume
+mkdir /nfs
+chown -R nfsnobody:nfsnobody /nfs
+chmod 700 -R /nfs
 
 # Make configurations
 echo '/nfs *(rw,all_squash)' >> /etc/exports
