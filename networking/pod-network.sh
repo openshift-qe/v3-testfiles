073cf0e93caa631fdea70b94a091a57ef0df1d86
bmeng <bmeng@redhat.com>
2015-11-19 18:26:02 +0800

    fix format

--- networking/pod-network.sh
+++ networking/pod-network.sh
@@ -2,108 +2,106 @@
 admin_conf=./admin.kubeconfig
 
 function access_in_pod() {
-	pod_ips=`for i in 1 2 3 4; do oc get po -o yaml -n bmengu1p$i ; done | grep podIP | cut -d ':' -f 2 | sed ':a;N;s/\n//g;ta'`
-	service_ips=`for i in 1 2 3 4 ; do oc get svc  -n bmengu1p$i ; done | awk '{print $2}' | grep -v CLUSTER | sed ':a;N;s/\n/ /g;ta'`
-	for i in 1 2 3 4
-	do 
-		oc project bmengu1p$i
-		pod_name=`oc get po -n bmengu1p$i| grep test-rc | sed -n 1p | cut -d ' ' -f1`
-		new_pod_name=`oc get po -n bmengu1p$i| grep new-test-rc | sed -n 1p | cut -d ' ' -f1`
-		oc exec -t $pod_name -- bash -c "for ip in $pod_ips ; do curl --connect-timeout 1 \$ip:8080 ; done" >> result.log
-		oc exec -t $new_pod_name -- bash -c "for ip in $service_ips ; do curl --connect-timeout 1 \$ip:27017 ; done" >> result.log
-		echo >> result.log
-	done	
+    pod_ips=`for i in 1 2 3 4; do oc get po -o yaml -n bmengu1p$i ; done | grep podIP | cut -d ':' -f 2 | sed ':a;N;s/\n//g;ta'`
+    service_ips=`for i in 1 2 3 4 ; do oc get svc  -n bmengu1p$i ; done | awk '{print $2}' | grep -v CLUSTER | sed ':a;N;s/\n/ /g;ta'`
+    for i in 1 2 3 4
+    do 
+        oc project bmengu1p$i
+        pod_name=`oc get po -n bmengu1p$i| grep test-rc | sed -n 1p | cut -d ' ' -f1`
+        new_pod_name=`oc get po -n bmengu1p$i| grep new-test-rc | sed -n 1p | cut -d ' ' -f1`
+        oc exec -t $pod_name -- bash -c "for ip in $pod_ips ; do curl --connect-timeout 1 \$ip:8080 ; done" >> pod_network_log
+        oc exec -t $new_pod_name -- bash -c "for ip in $service_ips ; do curl --connect-timeout 1 \$ip:27017 ; done" >> pod_network_log
+        echo >> pod_network_log
+    done	
 
 }
 
 function clean_account() {
-	oc delete project --all
-	sleep 15
+    oc delete project --all
+    sleep 15
 }
 
 function wait_for_running() {
-	while [ `for i in 1 2 3 4; do oc get po -n bmengu1p$i ; done | grep -v NAME | grep -v Running | wc -l` -ne 0 ]
-		do sleep 10
-	done
+    while [ `for i in 1 2 3 4; do oc get po -n bmengu1p$i ; done | grep -v NAME | grep -v Running | wc -l` -ne 0 ]
+        do sleep 10
+    done
 }
 
 function create_1st_round() {
-        for i in 1 2 3 4
-        do
+    for i in 1 2 3 4
+    do
                 oc new-project bmengu1p$i
                 oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/networking/list_for_pods.json -n bmengu1p$i
-		sleep 2
-        done
+    sleep 2
+    done
 	
-	sleep 10
+    sleep 10
 }
 
 function create_2nd_round() {
-        for i in 1 2 3 4
-        do
+    for i in 1 2 3 4
+    do
         curl -s https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/networking/list_for_pods.json | sed 's/test-/new-test-/g' | oc create -f - -n bmengu1p$i
- 	sleep 2
-        done
-	sleep 10
- 	wait_for_running       
+ 	    sleep 2
+    done
+    sleep 10
+    wait_for_running       
 }
 
 function pod_join_network() {
-	create_1st_round
-
-	oadm pod-network join-projects --to bmengu1p1 bmengu1p3 bmengu1p4 --config $admin_conf
+    create_1st_round
+    oadm pod-network join-projects --to bmengu1p1 bmengu1p3 bmengu1p4 --config $admin_conf
+    create_2nd_round
 	
-	create_2nd_round
+    echo >> pod_network_log	
 
-	echo > result.log	
-
-	access_in_pod
-	clean_account
-	mv result.log pod_join_network.log
+    access_in_pod
+    clean_account
+	
+    echo >> pod_network_log
 }
 
 function pod_join_network_by_selector() {
-        create_1st_round
+    create_1st_round
+    oc label ns bmengu1p2 ns=uni --config $admin_conf
+    oc label ns bmengu1p4 ns=uni --config $admin_conf
+    oadm pod-network join-projects --to bmengu1p1 --selector ns=uni --config $admin_conf
+    create_2nd_round
 
-	oc label ns bmengu1p2 ns=uni --config $admin_conf
-	oc label ns bmengu1p4 ns=uni --config $admin_conf
-	oadm pod-network join-projects --to bmengu1p1 --selector ns=uni --config $admin_conf
-        
-        create_2nd_round
+    echo >> pod_network_log
 
-        echo > result.log
-
-	access_in_pod
-	clean_account
-        mv result.log pod_join_network_selector.log
+    access_in_pod
+    clean_account
+    
+    echo >> pod_network_log
 }
 
 function pod_make_globel() {
-        create_1st_round
+    create_1st_round
+    oadm pod-network make-projects-global bmengu1p2 --config $admin_conf
+    create_2nd_round
+    
+    echo >> pod_network_log
 
-	oadm pod-network make-projects-global bmengu1p2 --config $admin_conf
-
-        create_2nd_round
-
-        echo > result.log
-	access_in_pod
-	clean_account
-        mv result.log pod_make_global.log
+    access_in_pod
+    clean_account
+    
+    echo >> pod_network_log
 }
 
 function pod_make_global_by_selector() {
-	create_1st_round
-	
-        oc label ns bmengu1p2 ns=uni --config $admin_conf
-        oc label ns bmengu1p4 ns=uni --config $admin_conf
-        oadm pod-network make-projects-global --selector ns=uni --config $admin_conf
+    create_1st_round
+    oc label ns bmengu1p2 ns=uni --config $admin_conf
+    oc label ns bmengu1p4 ns=uni --config $admin_conf
+    oadm pod-network make-projects-global --selector ns=uni --config $admin_conf
+    create_2nd_round
+    
+    echo >> pod_network_log
 
-	create_2nd_round
+    access_in_pod
+    clean_account
 
-	echo > result.log
-	access_in_pod
-	clean_account
-	mv result.log pod_make_global_by_selector.log
+    echo >> pod_network_log
 }
 
 
