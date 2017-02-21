63e4240e17709a24cc8d0b7cb9ffa358733bcdc2
Shiyang Wang <shiywang@redhat.com>
2017-02-21 16:47:35 +0800

    revert

--- templates/OCP-12297/trigger_deployment_failed.groovy
+++ templates/OCP-12297/trigger_deployment_failed.groovy
@@ -1,4 +1,4 @@
 node{
         stage 'build' 
-        def deploy = openshiftDeploy apiURL: '<repl_env>', authToken: '', depCfg: 'frontend', namespace: '<repl_ns>', verbose: 'false', waitTime: '1ms', waitUnit: 'sec'
+        def deploy = openshiftDeploy apiURL: '<repl_env>', authToken: '', depCfg: 'frontend', namespace: '<repl_ns>', verbose: 'false', waitTime: '1', waitUnit: 'sec'
 }
