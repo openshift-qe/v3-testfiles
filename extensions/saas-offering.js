dc9a1fb5197f4216ccd35eae1773e7ab6c96c599
yapei <yapei@redhat.com>
2017-12-11 16:48:13 +0800

    add test file saas-offering.js

---
+++ extensions/saas-offering.js
@@ -0,0 +1,14 @@
+// Remove one offering
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.pop();
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.pop();
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.pop();
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.pop();
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.pop();
+
+// Add offerings
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.push( {id: 1, title: 'UI Example App 1', icon: 'fa fa-cubes', url: 'https://www.google.com', description: 'this is added by uitester1'})
+// snipped
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.push( {id: 2, title: 'UI Example App 2', icon: 'fa fa-cubes', url: 'https://www.google.com', description: 'this is added by uitester2'})
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.push( {id: 3, title: 'UI Example App 3', icon: 'fa fa-cubes', url: 'https://www.google.com', description: 'this is added by uitester3'})
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.push( {id: 4, title: 'UI Example App 4', icon: 'fa fa-cubes', url: 'https://www.google.com', description: 'this is added by uitester4'})
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS.push( {id: 5, title: 'UI Example App 5', icon: 'fa fa-cubes', url: 'https://www.google.com', description: 'this is added by uitester5'})
