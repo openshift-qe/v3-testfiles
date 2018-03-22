5d6748e0d235eaa1108af1f427c9816aa73c4689
xiaocwan <xiaocwan@redhat.com>
2018-03-22 10:54:21 +0800

    Add quota-message.js for OCP-15418

---
+++ extensions/quota-message.js
@@ -0,0 +1,6 @@
+// Set custom notificaiton messages per quota type/key
+window.OPENSHIFT_CONSTANTS.QUOTA_NOTIFICATION_MESSAGE = {
+    "pods": 'Upgrade to <a href="http://www.online-pro1.com">OpenShift Online Pro1</a> if you need additional resources.',
+    "memory": 'Upgrade to <a href="http://www.online-pro2.com">OpenShift Online Pro2</a> if you need additional resources.'
+}
+
