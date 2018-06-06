217b0b5b82a45ca8dbcdf742cc6f3b7e1fc6a076
yapei <yapei@redhat.com>
2018-06-06 09:55:59 +0800

    add membership customization scripts

---
+++ extensions/membership.js
@@ -0,0 +1,13 @@
+window.OPENSHIFT_CONSTANTS.MEMBERSHIP_WHITELIST = [
+  "basic-user",
+  "edit",
+  "system:deployer",
+  "system:image-builder",
+  "system:image-puller",
+  "system:image-pusher",
+  "view",
+  "admin",
+  "yapei-role-1",
+  "yapei-role-2",
+  "deleteservices"
+];
---
+++ extensions/my-saas-offering.js
@@ -0,0 +1,17 @@
+// Add featured applications to the top of the catalog.
+window.OPENSHIFT_CONSTANTS.SAAS_OFFERINGS = [{
+  title: "Dashboard",                         // The text label
+  icon: "fa fa-dashboard",                    // The icon you want to appear
+  url: "http://example.com/dashboard",        // Where to go when this item is clicked
+  description: "Open application dashboard."  // Short description
+}, {
+  title: "System Status",
+  icon: "fa fa-heartbeat",
+  url: "http://example.com/status",
+  description: "View system alerts and outages."
+}, {
+  title: "Manage Account",
+  icon: "pficon pficon-user",
+  url: "http://example.com/account",
+  description: "Update email address or password."
+}];
