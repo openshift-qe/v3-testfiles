72dbde612f26c29f636d01801f29e4a8164cd20d
anli <anli@redhat.com>
2019-03-26 11:32:34 +0800

    Update the loggen files

--- logging/loggen/loggen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IMAGE=docker.io/mffiedler/ocp-logtest:latest
-PROJECT=${1:-log}
-oc new-project $PROJECT
-curl -O https://raw.githubusercontent.com/openshift/svt/master/openshift_scalability/content/logtest/logtest-rc.json
-oc new-app logtest-rc.json -n $PROJECT
-
-echo "Ref to https://github.com/openshift/svt/blob/master/openshift_scalability/content/logtest/ocp_logtest-README.md"
-
