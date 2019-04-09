1e325aea9236dcf3cfe592b10bffcfc8954107bd
anli <anli@redhat.com>
2019-04-09 22:08:50 +0800

    Update names

--- logging/fluentdserver/PasswordAuthentication/deploy.sh
+++ logging/fluentdserver/PasswordAuthentication/deploy.sh
@@ -2,9 +2,8 @@ oc create sa fluentdserver
 oc adm policy add-scc-to-user  privileged system:serviceaccount:openshift-logging:fluentdserver
 oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/PasswordAuthentication/fluentdserver_configmap.yaml
 oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/PasswordAuthentication/fluentdserver_deployment.yaml
-oc expose deployment/fluentdserver
-serviceip1=$(oc get service fluentdserver -o jsonpath={.spec.clusterIP})
-oc scale deployment cluster-logging-operator --replicas=0
+oc expose deployment/fluentdserver2
+serviceip1=$(oc get service fluentdserver2 -o jsonpath={.spec.clusterIP})
 #Add secret and update the configmap
 curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/PasswordAuthentication/fluentd_configmap_patch.yaml
 sed -i "s/192.168.1.2/${serviceip1}/" fluentd_configmap_patch.yaml
--- logging/fluentdserver/TLSMutualAuthentication/deploy.sh
+++ logging/fluentdserver/TLSMutualAuthentication/deploy.sh
@@ -2,9 +2,8 @@ oc create sa fluentdserver
 oc adm policy add-scc-to-user  privileged system:serviceaccount:openshift-logging:fluentdserver
 oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_configmap.yaml
 oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_deployment.yaml
-oc expose deployment/fluentdserver
-serviceip1=$(oc get service fluentdserver -o jsonpath={.spec.clusterIP})
-oc scale deployment cluster-logging-operator --replicas=0
+oc expose deployment/fluentdserver3
+serviceip1=$(oc get service fluentdserver3 -o jsonpath={.spec.clusterIP})
 #Add secret and update the configmap
 curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentd_configmap_patch.yaml
 sed -i "s/192.168.1.2/${serviceip1}/" fluentd_configmap_patch.yaml
