70cf352caa47cfd05a82d7c8914143cc28c71ead
anli <anli@redhat.com>
2019-04-09 19:49:12 +0800

    use different type of forward

--- logging/fluentdserver/TLSMutualAuthentication/deploy.sh
+++ logging/fluentdserver/TLSMutualAuthentication/deploy.sh
@@ -1,11 +1,11 @@
 oc create sa fluentdserver
 oc adm policy add-scc-to-user  privileged system:serviceaccount:openshift-logging:fluentdserver
-oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/forward/fluentdserver_deployment.yaml
+oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_configmap.yaml
+oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_deployment.yaml
 oc expose deployment/fluentdserver
 serviceip1=$(oc get service fluentdserver -o jsonpath={.spec.clusterIP})
 oc scale deployment cluster-logging-operator --replicas=0
 #Add secret and update the configmap
-curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/forward/fluentd_configmap_patch.yaml
+curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentd_configmap_patch.yaml
 sed -i "s/192.168.1.2/${serviceip1}/" fluentd_configmap_patch.yaml
 oc patch configmap/fluentd  --patch "$(cat fluentd_configmap_patch.yaml)"
-echo "oc exec $fluentdpod -- cat /fluentd/log/data.log"
