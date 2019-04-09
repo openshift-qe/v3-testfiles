b25c9c446fef5354ba0ab6e16e5923c03ae0ef32
anli <anli@redhat.com>
2019-04-09 23:32:37 +0800

    update the changes

--- logging/fluentdserver/TLSMutualAuthentication/deploy.sh
+++ logging/fluentdserver/TLSMutualAuthentication/deploy.sh
@@ -1,10 +1,25 @@
+curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_configmap.yaml
+curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_deployment.yaml
+curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentd_configmap_patch.yaml
+
 oc create sa fluentdserver
 oc adm policy add-scc-to-user  privileged system:serviceaccount:openshift-logging:fluentdserver
-oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_configmap.yaml
-oc create -f https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentdserver_deployment.yaml
+
+PASSPHRASE=oo_passphrase
+openssl req -new -x509 -sha256 -days 1095 -newkey rsa:2048 \
+              -keyout fluentd.key -out fluentd.crt
+
+oc create secret tls fluentdserver3 --cert=fluentd.cert --key=fluentd.key
+
+oc patch secrets/fluentd --type=json --patch "[{'op':'add','path':'/data/forward_ca_cert','value':'${cat fluentd.cert}'}]" 
+
+sed -i "s/oo_passphrase/${PASSPHRASE}" fluentdserver_configmap.yaml
+oc create -f fluentdserver_configmap.yaml
+oc create -f fluentdserver_deployment.yaml
 oc expose deployment/fluentdserver3
 serviceip1=$(oc get service fluentdserver3 -o jsonpath={.spec.clusterIP})
+
 #Add secret and update the configmap
-curl -O https://raw.githubusercontent.com/openshift-qe/v3-testfiles/master/logging/fluentdserver/TLSMutualAuthentication/fluentd_configmap_patch.yaml
 sed -i "s/192.168.1.2/${serviceip1}/" fluentd_configmap_patch.yaml
 oc patch configmap/fluentd  --patch "$(cat fluentd_configmap_patch.yaml)"
+oc delete fluentd --selector logging-infra=fluentd
