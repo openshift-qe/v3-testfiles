a2be28d35026e18dad646adf38790c9c77fe8031
anli <anli@redhat.com>
2019-10-08 12:37:40 +0800

    support different type of sync method

--- logging/tools/sync_images_to_internal_registry.sh
+++ logging/tools/sync_images_to_internal_registry.sh
@@ -2,6 +2,7 @@
 set -e
 from_registry=brew-pulp-docker01.web.prod.ext.phx2.redhat.com:8888
 to_registry=default-route-openshift-image-registry.apps.qeoanli.qe.devcluster.openshift.com
+method=${1:-oc}
 cur_dir=$PWD
 
 function syncImages()
@@ -32,22 +33,34 @@ echo "#sync image from internal regsitry to cluster"
 	    to_image_release=$to_registry/$image_name:$image_version-${image_release}
 	fi
 
-        #    echo "skip: this tool doesn't supoprt origin image"
-        #    continue
-        #    echo " step 1: docker pull $from_image"
-        #    docker pull $from_image
-        #    echo " step 2: docker tag $from_image $to_image_version"
-        #    docker tag $from_image $to_image_version
-        #    echo " step 3: docker push $to_image_version "
-        #    docker push $to_image_version
-        #    echo " step 2: docker tag $from_image $to_image_release"
-        #    docker tag $from_image $to_image_release
-        #    echo " step 3: docker push $to_image_release"
-        #    docker push $to_image_release
-	echo "# oc image mirror $from_image  $to_image_version --insecure=true"
-        oc image mirror $from_image  $to_image_version --insecure=true
-        echo "# oc image mirror $from_image  $to_image_release --insecure=true"
-        oc image mirror $from_image  $to_image_release --insecure=true
+        if [[ $method == "docker" ]]; then    
+            echo " step 1: docker pull $from_image"
+            docker pull $from_image
+            #echo " step 2: docker tag $from_image $to_image_version"
+            #docker tag $from_image $to_image_version
+            #echo " step 3: docker push $to_image_version "
+            #docker push $to_image_version
+            echo " step 4: docker tag $from_image $to_image_release"
+            docker tag $from_image $to_image_release
+            echo " step 5: docker push $to_image_release"
+            docker push $to_image_release
+         elif [[ $method == "podman" ]]; then
+            echo " step 1: podman pull $from_image"
+            podman pull $from_image --tls-verify=false
+            #echo " step 2: podman tag $from_image $to_image_version"
+            #podman tag $from_image $to_image_version
+            #echo " step 3: docker push $to_image_version  "
+            #podman push --cert-dir $Cert_Dir $to_image_version 
+            echo " step 4: podman tag $from_image $to_image_release"
+            podman tag $from_image $to_image_release
+            echo " step 5: podman push $to_image_release"
+            podman push --cert-dir $to_image_release
+	 else
+            #echo "# oc image mirror $from_image  $to_image_version --insecure=true"
+            #oc image mirror $from_image  $to_image_version --insecure=true
+            echo "# oc image mirror $from_image  $to_image_release --insecure=true"
+            oc image mirror $from_image  $to_image_release --insecure=true
+	fi
     done
 }
 
@@ -65,21 +78,35 @@ EOF
 oc apply -f /tmp/defaultRoute.yaml
 echo "#Configure cert and token to registry"
 to_registry=$(oc get images.config.openshift.io/cluster  -o jsonpath={.status.externalRegistryHostnames[0]})
+
+
+
+
+
 oc create serviceaccount registry |true
 oc adm policy add-cluster-role-to-user admin -z registry
-#oc get secret router-certs-default -n openshift-ingress -o json |jq -r '.data["tls.crt"]' | base64 -d >ca.crt
-#sudo cp ca.crt /etc/pki/ca-trust/source/anchors/${to_registry}.crt
-#sudo update-ca-trust enable
-#sudo systemctl daemon-reload
-#sudo systemctl restart docker
+oc get secret router-certs-default -n openshift-ingress -o json |jq -r '.data["tls.crt"]' | base64 -d >ca.crt
 
-echo "#Log in registry"
-echo docker login "${to_registry}" -u registry -p `oc sa get-token registry`
-docker login "${to_registry}" -u registry -p `oc sa get-token registry`
+if [[ $method == "podman" ]]; then
+    Cert_Dir=$HOME/.cert.d/${To_Registry}
+    mkdir -p $Cert_Dir
+    cp ca.crt $Cert_Dir/tls.crt
+    podman login "${To_Registry}" -u registry -p `oc sa get-token registry` --cert-dir $Cert_Dir
+else
+    sudo cp ca.crt /etc/pki/ca-trust/source/anchors/${to_registry}.crt
+    sudo update-ca-trust enable
+    sudo systemctl daemon-reload
+    sudo systemctl restart docker
+    echo "#Log in registry"
+    echo docker login "${to_registry}" -u registry -p `oc sa get-token registry`
+    docker login "${to_registry}" -u registry -p `oc sa get-token registry`
+fi
+
 if [[ $? != 0 ]]; then
     echo "Can not login cluster ${to_registry}"
     exit 1
 fi
+
 }
 
 ###########################Main##########################################
