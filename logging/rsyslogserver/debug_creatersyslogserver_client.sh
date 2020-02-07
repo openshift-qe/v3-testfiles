4adf20aef15edd735fd6a07ab1c62a32ea848ac6
anli <anli@redhat.com>
2020-02-07 10:53:11 +0800

    add debug client

---
+++ logging/rsyslogserver/debug_creatersyslogserver_client.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+
+cat <<EOF > rsyslogserver.yaml
+kind: Template
+apiVersion: v1
+metadata:
+  name: rsyslogserver-template
+  namespace: openshift-logging
+  annotations:
+    description: "A rsyslogserver to received clusterlogging forward messages."
+    tags: "rsyslogserver"
+objects:
+- kind: ConfigMap
+  apiVersion: v1
+  metadata:
+    name: \${NAME}-main
+    namespace: openshift-logging
+    labels:
+      provider: aosqe
+      component: "rsyslogserver"
+  data:
+    rsyslog.conf: |+
+      module(load="imfile" PollingInterval="10") #needs to be done just once
+      input(type="imfile"
+        File="/var/log/dnf.log"
+        Tag="tag1"
+        Severity="error"
+        Facility="local7")
+      #action(type="omfile" dirCreateMode="0700" FileCreateMode="0644"
+      #   File="/var/log/messages")
+      action(type="omfwd" target="rsyslogserver.openshift-logging.svc.cluster.local" port="514" protocol="tcp" queue.type="direct")
+      #action(type="omfwd" target="rsyslogserver.openshift-logging.svc.cluster.local" port="514" protocol="udp" queue.type="direct")
+      #action(type="omfwd" target="fd01::5:5cb6:62ff:fe00:b3" port="514" protocol="udp" queue.type="direct")
+
+- kind: ConfigMap
+  apiVersion: v1
+  metadata:
+    name: \${NAME}-bin
+    namespace: openshift-logging
+    labels:
+      provider: aosqe
+      component: "rsyslogserver"
+  data:
+    rsyslog.sh: "#!/bin/bash \n exec /usr/sbin/rsyslogd -f /etc/rsyslog/conf/rsyslog.conf -n"
+
+- kind: Deployment
+  apiVersion: extensions/v1beta1
+  metadata:
+    name:  \${NAME}
+    namespace: openshift-logging
+    labels:
+      provider: aosqe
+      component: "rsyslogserver"
+      appname: \${NAME}
+  spec:
+    replicas: 1
+    revisionHistoryLimit: 10
+    selector:
+      matchLabels:
+        provider: aosqe
+        component: "rsyslogserver"
+    strategy:
+      type: Recreate
+    template:
+      metadata:
+        labels:
+          provider: aosqe
+          component: "rsyslogserver"
+          appname: \${NAME}
+      spec:
+        serviceAccount: rsyslogserver
+        serviceAccountName: rsyslogserver
+        containers:
+        - name: "rsyslog"
+          args:
+          - /opt/app-root/bin/rsyslog.sh
+          command:
+          - /bin/sh
+          image: docker.io/viaq/rsyslog:latest
+          imagePullPolicy: IfNotPresent
+          securityContext:
+            privileged: true
+            procMount: Default
+          ports:
+          - containerPort: 514
+            name: rsyslog-pod-tcp
+            protocol: TCP
+          - containerPort: 514
+            name: rsyslog-pod-udp
+            protocol: UDP
+          volumeMounts:
+          - mountPath: /etc/rsyslog/conf
+            name: main
+            readOnly: true
+          - mountPath: /opt/app-root/bin
+            name: bin
+            readOnly: true
+        volumes:
+        - configMap:
+            defaultMode: 420
+            name: \${NAME}-main
+          name: main
+        - configMap:
+            defaultMode: 420
+            name: \${NAME}-bin
+          name: bin
+
+- apiVersion: v1
+  kind: Service
+  metadata:
+    annotations:
+      description: Exposes and load balances the application pods
+    labels:
+      provider: aosqe
+      component: "rsyslogserver"
+    name: \${NAME}
+  spec:
+    ports:
+    - name: rsyslogserver-tcp
+      port: 514
+      targetPort: 514
+      protocol: TCP
+    - name: rsyslogserver-udp
+      port: 514
+      targetPort: 514
+      protocol: UDP
+    selector:
+      appname: \${NAME}
+      provider: aosqe
+parameters:
+- description: The name assigned to all of the object 
+  displayName: Name
+  name: NAME
+  required: true
+  value: rsyslogserver
+EOF
+
+oc process -f  rsyslogserver.yaml -p NAME=rsyslogdebug | oc create -f -
